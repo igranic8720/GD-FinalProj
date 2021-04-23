8337cc42caa20069eed2fe9d97c1276ec40d8dc0
ivan <ivan.granic.ig@gmail.com>
2021-04-22 21:52:32 -0400

    FIXED DOORS. FUCKK

--- Assets/Scriots/DoorScript.cs
+++ Assets/Scriots/DoorScript.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,20 +6,24 @@ using UnityEngine;
 public class DoorScript : MonoBehaviour
 {
     private Animator animator;
+    public PhotonView PV;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        PV = gameObject.GetComponent<PhotonView>();
     }
 
     void Update()
     {
+        if (!PV.IsMine) { return; }
         GameObject[] objects = GameObject.FindGameObjectsWithTag("Player");
         foreach (GameObject py in objects)
         {
             if (Vector3.Distance(py.transform.position, transform.position) <= 4f)
             {
                 animator.SetBool("character_nearby", true);
+                break;
             }
             else
             {
